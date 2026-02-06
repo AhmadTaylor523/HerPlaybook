c13c4ff68f92bc115015887c44195240101546a8
AhmadTayl <ataylor9@ithaca.edu>
2026-02-06 10:38:35 -0500

    Initial Commit

---
+++ HerPlaybook/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HerPlaybook
+//
+//  Created by Ahmad Taylor on 2/6/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HerPlaybookTests/HerPlaybookTests.swift
@@ -0,0 +1,17 @@
+//
+//  HerPlaybookTests.swift
+//  HerPlaybookTests
+//
+//  Created by Ahmad Taylor on 2/6/26.
+//
+
+import Testing
+@testable import HerPlaybook
+
+struct HerPlaybookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
